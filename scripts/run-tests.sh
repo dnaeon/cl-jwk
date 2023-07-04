e04d9774b22c854c4e870a8b50297a43c803ed51
Marin Atanasov Nikolov <dnaeon@gmail.com>
2023-07-04 18:25:06 +0300

    scripts: add no-debug flags for SBCL and ECL while testing

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,7 +4,18 @@ set -e
 
 LISP=${LISP:-sbcl}
 
-${LISP} --eval '(ql:quickload :cl-jwk.test)' \
+_no_debug_flag=""
+case "${LISP}" in
+    sbcl)
+        _no_debug_flag="--non-interactive"
+        ;;
+    ecl)
+        _no_debug_flag="--nodebug"
+        ;;
+esac
+
+${LISP} "${_no_debug_flag}" \
+        --eval '(ql:quickload :cl-jwk.test)' \
         --eval '(setf rove:*enable-colors* nil)' \
         --eval '(asdf:test-system :cl-jwk.test)' \
         --eval '(uiop:quit (length (rove/core/stats:all-failed-assertions rove/core/stats:*stats*)))'
